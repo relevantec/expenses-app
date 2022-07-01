6df53e92e4ec71ba418729addbc1b1995b0f50aa
JZhelyazkov91 <zhelyazkov@leandigitalsolutions.com>
2022-07-01 10:10:11 +0300

    First commit

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -43,54 +43,13 @@ export class AuthService {
     console.log(body.toString());    
 
     let headers =  new HttpHeaders({
-        // 'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Bearer ' + encodeURIComponent('SLcHBNZSy7ZIIFA56AwR7GG23xvbdI')
-        // Authorization: 'Basic ' + encodeURIComponent('SLcHBNZSy7ZIIFA56AwR7GG23xvbdI')
       })
     let options = { headers };
     
 
     return this.http.post<any>('https://www.marketcircle.com/account/oauth/token',
       body, options);
-      //   tap(res => {
-      //     console.log(body);
-          
-      //     this.tokenService.saveToken(res.access_token);
-      //     this.tokenService.saveRefreshToken(res.refresh_token);
-      //   }),
-        // catchError(this.handleError)
-      // );
   }
 
-  // refreshToken(refreshData: any): Observable<any> {
-  //   const body = new HttpParams()
-  //     .set('client_secret', encodeURIComponent("mLVRSUUxY8tSHrrQhYqK"))
-  //     .set('client_id', encodeURIComponent(this.id))
-  //     .set('authorization_code', encodeURIComponent(this.code))
-  //     .set('grant_type', encodeURIComponent("authorization_code"));
-
-  //   const httpOptions = {
-  //     headers: new HttpHeaders({
-  //       'Content-Type': 'application/x-www-form-urlencoded',
-  //       Authorization: 'Bearer ' + encodeURIComponent('SLcHBNZSy7ZIIFA56AwR7GG23xvbdI')
-  //     })
-  //   }
-  //   this.tokenService.removeToken();
-  //   this.tokenService.removeRefreshToken();
-  //   const bodyRefr = new URLSearchParams({
-  //     'refresh_token': refreshData.refresh_token,
-  //     'grant_type': 'refresh_token'
-  //   })
-  //   return this.http.post<any>('https://www.marketcircle.com/account/oauth/token',
-  //   bodyRefr, httpOptions)
-  //     .pipe(
-  //       tap(res => {
-  //         console.log(res);
-  //         this.tokenService.saveToken(res.access_token);
-  //         this.tokenService.saveRefreshToken(res.refresh_token);
-  //       }),
-  //       catchError(this.handleError)
-  //     );
-  // }
 }
